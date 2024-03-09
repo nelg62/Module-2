578fca6fd77179ed96281bce643101e84fa8f785
nelg62 <glen.harding.nz@gmail.com>
2024-03-09 15:23:20 +1300

    changed documentgetid to query selector

--- number guesser/numberguess.js
+++ number guesser/numberguess.js
@@ -1,35 +1,52 @@
+// create random number
 let secretnumber = Math.floor(Math.random() * 10) + 1;
 
-const form = document.querySelector('form')
+// create form variable with query selector
+const form = document.querySelector("form");
 
-let choosennumbers = []
+// create result variable with query selector
+const result = document.querySelector("#output");
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    const formData = new FormData(form)
+// create empty choosen number object
+let choosennumbers = [];
 
-    const obj = Object.fromEntries(formData)
+// form prevent default
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  //   create variable formdata from data from form
+  const formData = new FormData(form);
 
-    choosennumbers.push(obj.numchoice)
-    
-    
-    document.getElementById('chosenhistory').innerHTML = `You have chosen: ${choosennumbers}`
-    document.getElementById('output').innerHTML = obj.numchoice
+  //   create variable and put object data from fromdata into this
+  const obj = Object.fromEntries(formData);
 
-    console.log(secretnumber)
-    console.log(obj.numchoice)
-    console.log(choosennumbers)
+  console.log(secretnumber);
+  console.log(obj.numchoice);
+  console.log(choosennumbers);
 
-    if (secretnumber == obj.numchoice) {
-        document.getElementById('output').innerHTML = "Good Work"
-    }else {
-        document.getElementById('output').innerHTML = "Not matched"
+  numcheck();
+
+  // check if number chosen is > 10 < 1 or nothing then check if number is = to the secret number
+  function numcheck() {
+    if (obj.numchoice < 1 || obj.numchoice > 10 || obj.numchoice == "") {
+      result.textContent = "Please cjhoose a number between 1 and 10";
+    } else if (secretnumber == obj.numchoice) {
+      result.textContent = `Good Work the number was ${secretnumber} you got it after ${choosennumbers.length + 1} guesses`;
+    } else {
+        //   push numbercoice from input to object choosennumbers
+      choosennumbers.push(obj.numchoice);
+      //  change text of p element chosenhistory to number chouces
+      document.getElementById(
+        "chosenhistory"
+      ).innerHTML = `You have chosen: ${choosennumbers}`;
+      result.textContent = "Not matched";
     }
-})
+  }
+});
 
+// refresh remove values and refresh random number
 function refresh() {
-    secretnumber = Math.floor(Math.random() * 10) + 1;
-    choosennumbers = []
-    document.getElementById('chosenhistory').innerHTML = ""
-    document.getElementById('output').innerHTML = ""
+  secretnumber = Math.floor(Math.random() * 10) + 1;
+  choosennumbers = [];
+  document.getElementById("chosenhistory").innerHTML = "";
+  result.textContent = "";
 }
